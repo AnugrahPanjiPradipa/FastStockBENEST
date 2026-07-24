99411f6e10b3f264bd7f0ebeb12ba07095991fbf
AnugrahPanjiPradipa <panjipradipa714@gmail.com>
2026-07-24 17:12:59 +0700

    Update app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHealthCheck() {
+    return {
+      status: 'online',
+      message: 'FastStock NestJS API is running smoothly!',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
